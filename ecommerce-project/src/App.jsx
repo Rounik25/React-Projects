9178e0cf7b9d87326ace0dd4efbe8f5e33b93681
Rounik Jaiswal <rounik.jaiswal@bain.com>
2026-02-19 18:16:53 +0530

    moved checkout page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,17 @@
 import { HomePage } from './pages/HomePage'
+import { Checkout } from './pages/Checkout'
 import './App.css'
+import { Routes , Route } from 'react-router'
 
 function App() {
   
 
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='checkout' element={<Checkout />}></Route>
+      </Routes>
     </>
   )
 }
