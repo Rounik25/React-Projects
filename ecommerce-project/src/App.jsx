3be98ccf9ca512f962bdfd94848b004483936381
Rounik Jaiswal <rounik.jaiswal@bain.com>
2026-02-20 10:28:43 +0530

    updated home page to fetch images and ratings of products from data file

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 import './App.css';
 import { Routes , Route } from 'react-router';
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='checkout' element={<CheckoutPage />} />
         <Route path='orders' element={<OrdersPage />} />
+        <Route path='tracking' element={<TrackingPage />} />
       </Routes>
     </>
   )
