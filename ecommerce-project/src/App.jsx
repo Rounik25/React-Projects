de6cdef6e4c142ce29562f8773609febe694a348
Rounik Jaiswal <rounik.jaiswal@bain.com>
2026-02-20 12:02:01 +0530

    Updated cart to fetch items from backend

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -4,15 +4,22 @@ import { OrdersPage } from './pages/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
 import './App.css';
 import { Routes , Route } from 'react-router';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  
-
+    const [cart,setCart]=useState([]);
+    useEffect(()=>{
+      axios.get('/api/cart-items?expand=product')
+            .then((response)=>{
+                setCart(response.data);
+        });
+    },[]);
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='checkout' element={<CheckoutPage />} />
+        <Route path='/' element={<HomePage cart={cart} />} />
+        <Route path='checkout' element={<CheckoutPage cart={cart} />} />
         <Route path='orders' element={<OrdersPage />} />
         <Route path='tracking' element={<TrackingPage />} />
       </Routes>
