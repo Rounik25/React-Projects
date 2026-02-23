88e4a6515fe47e085d52041530b8aed2f3238a19
Rounik Jaiswal <rounik.jaiswal@bain.com>
2026-02-23 14:42:40 +0530

    feature to add new orders from cart

--- ecommerce-project/src/components/checkout/PaymentSummary.jsx
+++ ecommerce-project/src/components/checkout/PaymentSummary.jsx
@@ -1,4 +1,15 @@
-export function PaymentSummary({paymentSummary}) {
+import axios from "axios";
+import { useNavigate } from "react-router";
+
+export function PaymentSummary({paymentSummary,loadCart}) {
+    const navigate = useNavigate();
+
+    const createOrder = async () => {
+        await axios.post('/api/orders');
+        await loadCart();
+        navigate('/orders');
+    }
+
     return (
         <div className="payment-summary">
             <div className="payment-summary-title">
@@ -31,7 +42,8 @@ export function PaymentSummary({paymentSummary}) {
                         <div className="payment-summary-money">{`$${(paymentSummary.totalCostCents / 100).toFixed(2)}`}</div>
                     </div>
 
-                    <button className="place-order-button button-primary">
+                    <button className="place-order-button button-primary"
+                    onClick={createOrder}>
                         Place your order
                     </button>
                 </>
